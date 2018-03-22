c6b7a0f6b5726bebeed0ddd3323ab1ee82108b48
Lasse Agerbo Frederiksen <Lasse@jdoe.e2015.au.dk>
2018-03-22 08:22:07 +0100

    Changed the code over to a header file for ease of adding to other places

---
+++ Sqlitetest/src/Sqlitecode.h
@@ -0,0 +1,19 @@
+/*
+ * Sqlitecode.h
+ *
+ *  Created on: 22 Mar 2018
+ *      Author: Lasse
+ */
+
+#ifndef SRC_SQLITECODE_H_
+#define SRC_SQLITECODE_H_
+
+void Database_setup(int argc, char* argv[]);
+
+void Database_insert(int argc, char* argv[]);
+
+void Database_select(int argc, char* argv[]);
+
+void Database_update(int argc, char* argv[]);
+
+#endif /* SRC_SQLITECODE_H_ */
